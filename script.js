5785e95417dba6a80396d37230d6b9ef231f305f
katherine rodriguez <katherine.rodriguezz.ny@gmail.com>
2022-10-10 22:34:16 -0400

    first commit

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
   //declare a variable named monsterHealth and set it equal to the number value 15
   let monsterHealth = 15
 
+
 //random integer function 
 //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function randomNum(min, max) {
