5d66b0267bbdcb8f6a4663cbe1d77c46348cd25a
katherine rodriguez <katherine.rodriguezz.ny@gmail.com>
2022-10-25 21:38:09 -0400

    second commit

--- script.js
+++ script.js
@@ -21,32 +21,82 @@ function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
 }
 
-function playerAttack(){
-//use randomNum to generate attack points value between 1 - 5 and save the value to a variable named playerAttackPoints
-let playerAttackPoints = randomNum(1, 6);
-monsterHealth = monsterHealth - playerAttackPoints;
-alert(`${playerName} has viciously attacked ${monsterName}. 
-The attack left ${playerAttackPoints} of damage resulting in ${monsterHealth} health points.`)
+///fighter class is parent to Hero and Monster
+//Fighter contains everything that both Hero and Monster have - method, properties
+
+class Fighter{
+  constructor(name){
+    this.name = name
+    this.healthPoints = 15
+  }
+  //hero and mnster will both have attack method
+  attack(targetObj){
+    let attackPoints = randomNum(1,6);
+    targetObj.healthPoints = targetObj.healthPoints - attackPoints;
+    return `${this.name} attacked ${targetobj.name}! ${this.name} did ${attackPoints} damage. ${targetObj.name}
+    has ${targetObj.healthPoints} health points left`
+  }
+}
+
+class Monster extends Fighter{
+  constructor(name, superPowers){
+    super(name);
+    this.superPowers = superPowers;
+  }
+}
+
+let logicalBeast = new Monster("logicalBeast", "shoot with brain")
+
+class Hero extends Fighter{
+  constructor(name, weapon){
+    super(name);
+    this.weapon = weapon
+  }
+}
+let knight= new Hero("Knight", "sword");
+
+if (!zeroOrone) {
+  Hero.attack(monster);
+  if (monster.healthPoints > 0) {
+    monster.attack(hero);
+  }
+}
+
+else {
+  monster.attack(hero)
+  if(hero.healthPoints >0){
+    hero.attack(monster)
+  }
 }
 
 
 
-function monsterAttack(){
+//function playerAttack(){
+//use randomNum to generate attack points value between 1 - 5 and save the value to a variable named playerAttackPoints
+//let playerAttackPoints = randomNum(1, 6);
+//monsterHealth = monsterHealth - playerAttackPoints;
+//alert(`${playerName} has viciously attacked ${monsterName}. 
+//The attack left ${playerAttackPoints} of damage resulting in ${monsterHealth} health points.`)
+//}
+
+
+
+//function monsterAttack(){
   //use randomNum to generate attack points value between 1 - 5 and save the value to a variable named monsterAttackPoints
-  let monsterAttackPoints = randomNum(1, 6);
+  //let monsterAttackPoints = randomNum(1, 6);
 
   //subtract monsterAttackPoints from playerHealth and update the playerHealth variable 
-  playerHealth = playerHealth - monsterAttackPoints;
+ // playerHealth = playerHealth - monsterAttackPoints;
 
   //use an alert with string template literals to tell the player: 
 
-  alert(`Monster attacked player and did ${monsterAttackPoints} of damage! ${playerName} has ${playerHealth} health left!`)
+  //alert(`Monster attacked player and did ${monsterAttackPoints} of damage! ${playerName} has ${playerHealth} health left!`)
   // 1. monster attacked player 
   // 2. how much damage the monster did 
   // 3. how much health the player has 
-}
+//}
 
-function playRound() {
+/*function playRound() {
   //use randomNum to return either 0 or 1
   randomNum(0,2)
   
@@ -61,32 +111,32 @@ function playRound() {
       playerAttack();
     } 
   }
-}
+}*/
 
-function playGame() {
+function playGame(hero, monster) {
   //beginning game message
   alert(
-    `Hello, ${playerName}! You are fighting ${monsterName}! 
-    Your health is at ${playerHealth}, ${monsterName}'s health is at ${monsterHealth}`
+    `Hello, ${hero.Name}! You are fighting ${monster.Name}! 
+    Your health is at ${hero.Health}, ${monster.Name}'s health is at ${monster.Health}`
   );
 
  let roundNumber = 0;
 
   //while loop that runs until player or monster's health is <= 0 
   //add the condition in the while loop parentheses 
-  while( (monsterHealth > 0)  && (playerHealth > 0) ){
+  while( (monster.healthPoints > 0)  && (hero.healthPoints > 0) ){
     roundNumber++;
    //write an alert statement that tells the player what round number it is, and the player's and monster's current health points
-    alert(`This is round number ${roundNumber}, ${playerName} has ${playerHealth} health and ${monsterName} has ${monsterHealth}`);
+    alert(`This is round number ${roundNumber}, ${hero.name} has ${hero.healthPoints} health and ${monsterName} has ${monster.healthPoints}`);
    //call playRound inside the while loop
    playRound();
     
   }
   //outside of while loop, declare a winner and use alert to show a win or lose message 
-  if (playerHealth < 0) {
-    alert(`${monsterName} WINS! Sorry, you lose!`);
-  } else if (monsterHealth < 0) {
-    alert(`${playerName} WINS! Congratulations, you won!`)
+  if (hero.healthPoints < 0) {
+    alert(`${hero.name} WINS! Sorry, you lose!`);
+  } else if (monster.name < 0) {
+    alert(`${hero.Name} WINS! Congratulations, you won!`)
   }
 }
 
